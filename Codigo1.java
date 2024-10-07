8672ef186dd85df1a4dd872a9b8167096dd9a1ed
Matheus Castanha <matheuscastrod33@gmail.com>
2024-10-07 20:22:30 -0300

    Create Codigo1.java

---
+++ Codigo1.java
@@ -0,0 +1,18 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    int a=4 , b=3, c=2, r;
+    r = (a + b) * c;
+    
+    System.out.println("A soma dos valores é: "+r);
+    
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
