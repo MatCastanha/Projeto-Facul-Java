f57e417f0b9c4c750cc6594f4476908a0bc53f8e
Matheus Castanha <matheuscastrod33@gmail.com>
2024-10-07 20:38:32 -0300

    Create Codigo2.java

---
+++ Aula 07.10,24/Codigo2.java
@@ -0,0 +1,20 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    int idade = 18;
+    String nome = "Matheus";
+    boolean estudante = true;
+    double altura = 1.76;
+    
+    System.out.println("Eu sou "+nome+" tenho "+ idade+" minha altura é "+altura+ "\nEu sou estudante? "+ estudante);
+    
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
