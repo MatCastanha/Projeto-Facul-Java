df4f2b553fa505a5303dc1e521b146d75d9480df
Matheus Castanha <matheuscastrod33@gmail.com>
2024-10-07 21:03:22 -0300

    Create Codigo3.java

---
+++ Aula 07.10,24/Codigo3.java
@@ -0,0 +1,19 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args){
+    
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.println("Digite Sua Idade:");
+    int idade = scanner.nextInt();
+    System.out.println("Sua Idade e: " + idade);
+    
+  }
+
+  
+}
