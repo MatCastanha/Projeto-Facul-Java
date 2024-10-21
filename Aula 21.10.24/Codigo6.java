0d104e5034b0216f85eb82f29e95b6e94c6db025
Matheus Castanha <matheuscastrod33@gmail.com>
2024-10-21 19:35:16 -0300

    Create Codigo6.java

---
+++ Aula 21.10.24/Codigo6.java
@@ -0,0 +1,23 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    
+    double area=0, altura=0, base=0;
+   
+    
+    System.out.println("A bese e a altura do retangulo");
+    base = scanner.nextDouble();
+    altura = scanner.nextDouble();
+
+    area = base * altura;
+
+    System.out.print("A área do retangulo é de: "+ area);
+  }
+  
+}
