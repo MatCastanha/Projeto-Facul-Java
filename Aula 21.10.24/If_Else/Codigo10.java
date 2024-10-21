590fec7eba2e6a8dab614491fc429dbf92c7c92b
Matheus Castanha <matheuscastrod33@gmail.com>
2024-10-21 20:33:31 -0300

    Create Codigo10.java

---
+++ Aula 21.10.24/If_Else/Codigo10.java
@@ -0,0 +1,32 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+
+    double Preco=0, Desconto=0;
+      
+    System.out.print("Digite O valor do produto: ");
+    Preco = scanner.nextDouble();
+
+    if(Preco<50){
+      System.out.print("O preço final do produto é de: "+Preco);
+    }
+    else if(Preco<100){
+      Desconto = Preco * 0.1;
+      Preco = Preco - Desconto;
+      System.out.print("O preço final do produto com 10% de desconto é de: "+Preco);
+    }
+    else{
+      Desconto = Preco * 0.2;
+      Preco = Preco - Desconto;
+      System.out.print("O preço final do produto com 20% de desconto é de: "+ Preco);
+    }
+
+  }
+  
+}
