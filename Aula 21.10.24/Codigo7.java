2c41c8027781480e88881da72474023b18cd7b26
Matheus Castanha <matheuscastrod33@gmail.com>
2024-10-21 19:38:47 -0300

    Create Codigo7.java

---
+++ Aula 21.10.24/Codigo7.java
@@ -0,0 +1,22 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    
+    double area=0, raio=0;
+   
+    
+    System.out.println("Digite o raio do circulo");
+    raio = scanner.nextDouble();
+
+    area = 3.14 * (raio*raio);
+
+    System.out.print("A área do circulo é de: "+ area);
+  }
+  
+}
