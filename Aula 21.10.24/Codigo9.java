0aab54f06291f5e51c029a1120b1b53d8a519da9
Matheus Castanha <matheuscastrod33@gmail.com>
2024-10-21 19:50:25 -0300

    Create Codigo9.java

---
+++ Aula 21.10.24/Codigo9.java
@@ -0,0 +1,25 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    
+    double Valor=0, Desconto=0;
+   
+    
+    System.out.println("Digite o Valor do produto e quanto de desconto ele tem:");
+    Valor = scanner.nextDouble();
+    Desconto = scanner.nextDouble();
+
+    Desconto = Desconto / 100;
+
+    Valor = Valor - (Valor * Desconto);
+      
+    System.out.print("O valor final do produto é de: "+Valor);
+  }
+  
+}
