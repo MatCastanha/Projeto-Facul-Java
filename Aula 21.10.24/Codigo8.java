26a97425f6faaa93761f2aa61fdad71797199936
Matheus Castanha <matheuscastrod33@gmail.com>
2024-10-21 19:43:26 -0300

    Create Codigo8.java

---
+++ Aula 21.10.24/Codigo8.java
@@ -0,0 +1,22 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    
+    double Celc=0, Fahr=0;
+   
+    
+    System.out.println("Digite a temperatura:");
+    Celc = scanner.nextDouble();
+
+    Fahr = ((Celc * 9) / 5) + 32;
+
+    System.out.print("A Temperatura em Fahrenheit é de: "+Fahr );
+  }
+  
+}
