e17d5375dd75919a9a1db821fcbdf8d61566f0fe
Matheus Castanha <matheuscastrod33@gmail.com>
2024-10-21 20:54:34 -0300

    Create Codigo13.java

---
+++ Aula 21.10.24/If_Else/Codigo13.java
@@ -0,0 +1,25 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+
+    int media=0;
+
+    System.out.print("Digite sua média: ");
+    media = scanner.nextInt();
+
+    if(media<6){
+      System.out.print("Você está reprovado");
+    }
+    else{
+      System.out.print("Você está Aprovado");
+    }
+
+  }
+  
+}
