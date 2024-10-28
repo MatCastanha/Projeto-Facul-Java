f4cda24cda86acb4d71d08e5befc697ce06096be
Matheus Castanha <matheuscastrod33@gmail.com>
2024-10-28 20:19:49 -0300

    Create Contador Decresente.java

---
+++ Aula 28.10.24/Atv While/Contador Decresente.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+
+    int i=30; 
+    while(i>=1){
+      System.out.println(i);
+      i--;
+    }
+    System.out.print("Acabou");
+    
+  }
+  
+}
