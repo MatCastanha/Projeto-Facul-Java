061004b52c5474223664c8a4cfb801ef98e70456
Matheus Castanha <matheuscastrod33@gmail.com>
2024-10-31 13:46:39 -0300

    Update and rename Codigo2.java to Variaveis.java

--- Aula 07.10,24/Variaveis.java
+++ Aula 07.10,24/Variaveis.java
@@ -1,7 +1,3 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
-
 public class Main {
   public static void main(String[] args) {
     int idade = 18;
@@ -12,9 +8,4 @@ public class Main {
     System.out.println("Eu sou "+nome+" tenho "+ idade+" minha altura é "+altura+ "\nEu sou estudante? "+ estudante);
     
   }
-
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
 }
