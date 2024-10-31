0dc1556eea8854ba09aae1a0c4532f958af70b9a
Matheus Castanha <matheuscastrod33@gmail.com>
2024-10-31 13:45:14 -0300

    Update and rename Codigo1.java to Calculo.java

--- Aula 07.10,24/Calculo.java
+++ Aula 07.10,24/Calculo.java
@@ -1,7 +1,3 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
-
 public class Main {
   public static void main(String[] args) {
     int a=4 , b=3, c=2, r;
@@ -10,9 +6,4 @@ public class Main {
     System.out.println("A soma dos valores é: "+r);
     
   }
-
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
 }
