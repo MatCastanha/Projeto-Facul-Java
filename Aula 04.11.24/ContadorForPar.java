8446a15cf3014754a29dd2eebcfccc5ea27c5ae5
Matheus Castanha <matheuscastrod33@gmail.com>
2024-11-04 19:47:04 -0300

    Create ContadorForPar.java

---
+++ Aula 04.11.24/ContadorForPar.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static void main(String[] args){
+
+      int i;
+          
+      for(i=0; i<151; i++){
+          
+          if (i%2==0) {
+              System.out.println(i);
+        }
+          
+      }
+      
+  }
+
+} 
