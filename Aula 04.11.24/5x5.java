1c3f318b0b58790f997f44f53fa54ec9e9cc0996
Matheus Castanha <matheuscastrod33@gmail.com>
2024-11-04 20:58:41 -0300

    Update 5x5.java

--- Aula 04.11.24/5x5.java
+++ Aula 04.11.24/5x5.java
@@ -2,27 +2,11 @@ public class Main {
   public static void main(String[] args){
      
       for(int i=0; i<5; i++){
-        System.out.print("* ");
-      }
-    System.out.println();
-      
-      for(int i=0; i<5; i++){
-          System.out.print("* ");
-        }  
-    System.out.println();
-      for(int i=0; i<5; i++){
-          System.out.print("* ");
-        }
-
-    System.out.println();
-        for(int i=0; i<5; i++){
+        for(int s=0; s<5;s++){
             System.out.print("* ");
         }
-      
-      System.out.println();
-        for(int i=0; i<5; i++){
-            System.out.print("* ");
-        }
+        System.out.println();
+      }  
   }
 
 } 
