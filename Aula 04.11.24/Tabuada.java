33a2a80dc4f06326603c6831d47e66762a65ecdb
Matheus Castanha <matheuscastrod33@gmail.com>
2024-11-04 19:55:16 -0300

    Create Tabuada.java

---
+++ Aula 04.11.24/Tabuada.java
@@ -0,0 +1,15 @@
+public class Main {
+  public static void main(String[] args){
+
+      int i, result=0;
+      
+      for(i=1; i<11; i++){
+          
+          result = 3 * i;
+          System.out.println("3 x "+ i +" = " + result);
+          
+      }
+      
+  }
+
+} 
